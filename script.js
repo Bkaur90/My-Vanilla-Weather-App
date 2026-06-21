f526f8df3379d3f0bae8bde004232399104bfb29
Bkaur90 <baljeetkaler3019@gmail.com>
2026-06-21 00:17:02 -0400

    Added SearchEngine JS

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function ChangeCityName(event) {
+  event.preventDefault();
+  let cityname = document.querySelector("#city");
+  let citySearchInput = document.querySelector("#search-input");
+  cityname.innerHTML = citySearchInput.value;
+}
 
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", ChangeCityName);
