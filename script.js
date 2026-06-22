5ff09d9d1aece64312f224f36bc35980766c8be1
Bkaur90 <baljeetkaler3019@gmail.com>
2026-06-21 23:40:36 -0400

    Added API

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+function ChangeTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let ChangeTemperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(ChangeTemperature);
+    let cityname = document.querySelector("#city");
+    let citySearchInput = document.querySelector("#search-input");
+  cityname.innerHTML = response.data.city;
+
+}
+
+function ApplyCityAPI(city) {
+  let apiKey = "ff2o8b1ffc14490t1c0bba91e21ca53c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(ChangeTemperature);
+}
+
 function ChangeCityName(event) {
   event.preventDefault();
   let cityname = document.querySelector("#city");
   let citySearchInput = document.querySelector("#search-input");
-  cityname.innerHTML = citySearchInput.value;
+
+  ApplyCityAPI(citySearchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
